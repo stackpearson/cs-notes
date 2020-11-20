04d16ca95a214a53f07024d3f6865c625b2a367e
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-11-20 15:31:09 -0700

    insertAfter done

---
+++ linked_list_construction.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+		if self.head is None:
+			self.head = node
+			self.tail = node
+			return
+		self.insertBefore(self.head, node)
+
+    def setTail(self, node):
+		if self.tail is None:
+			self.setHead(node)
+			return
+		self.insertAfter(self.tail, node)
+
+    def insertBefore(self, node, nodeToInsert):
+		if nodeToInsert == self.head and nodeToInsert == self.tail:
+			return
+		self.remove(nodeToInsert)
+		nodeToInsert.prev = node.prev
+		nodeToInsert.next = node
+		if node.prev is None:
+			self.head = nodeToInsert
+		else:
+			node.prev.next = nodeToInsert
+		node.prev = nodeToInsert
+
+    def insertAfter(self, node, nodeToInsert):
+		if nodeToInsert == self.head and nodeToInsert == self.tail:
+			return
+		self.remove(nodeToInsert)
+		nodeToInsert.prev = node
+		nodeToInsert.next = node.next
+		if node.next is None:
+			self.tail = nodeToInsert
+		else:
+			node.next.prev = nodeToInsert
+		node.next = nodeToInsert

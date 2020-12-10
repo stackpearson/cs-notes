8b8dcdeb24e5074fc7ca4064e4bb05dab5f1fc6e
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-12-10 13:32:34 -0700

    restructured files and added reversing a linked list

---
+++ sprint_2/reverseLinkedList.py
@@ -0,0 +1,11 @@
+def reverseLinkedList(head):
+	prevPointer, midPointer = None, head
+	
+	while midPointer is not None:
+		nextPointer = midPointer.next
+		midPointer.next = prevPointer
+		prevPointer = midPointer
+		midPointer = nextPointer
+	return prevPointer
+
+    # also see https://www.algoexpert.io/questions/Reverse%20Linked%20List

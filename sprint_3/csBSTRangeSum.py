5a0c69f46e57ce97f2561cfd8d35ead6e2cc11e1
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-12-14 11:49:32 -0700

    csBSTRangeSum complete

---
+++ sprint_3/csBSTRangeSum.py
@@ -0,0 +1,14 @@
+def csBSTRangeSum(root, lower, upper):
+    values = []
+    helper(root, values, lower, upper)
+
+    return sum(values)
+    
+def helper(root, values, lower, upper):
+    if root is None:
+        return
+    else:
+        helper(root.left, values, lower, upper)
+        helper(root.right, values, lower, upper)
+        if (root.value >= lower) and (root.value <= upper):
+            values.append(root.value)

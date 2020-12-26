2521a30deab0f7a9ebb1f8763a887eba8365baf5
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-12-25 17:20:15 -0700

    updated instructions for seatsInTheater

--- codesignal_arcade/seatsInTheater.py
+++ codesignal_arcade/seatsInTheater.py
@@ -1,2 +1,4 @@
 def seatsInTheater(nCols, nRows, col, row):
     return (nRows - row) * (nCols - col + 1)
+
+# this question was asking giving you a grid of theather seats and asking you to find the number of people direclty behind you. nCols was the # of columns, nRows = number of rows, col was your column & row was your row.

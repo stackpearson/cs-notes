7b71f5e5c807724b2db62e5fe9838c0e792b2af7
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-12-26 17:33:13 -0700

    updated isLucky

--- codesignal_arcade/isLucky.py
+++ codesignal_arcade/isLucky.py
@@ -5,3 +5,5 @@ def isLucky(n):
     # print('right', right)
     # print('left', left)
     return sum(map(int, left)) == sum(map(int, right)
+
+# this question gives a string of digits, it's asking you to check to see if the # of digits on either side is even or odd. If odd, it's not lucky if even, it is lucky.
